62b47d00640b70b746f1dc5021c783f23f270631
Mustafa Shubbar <codingbox@outlook.com>
2021-01-11 00:15:55 -0500

    init

---
+++ src/musicplaylist/MusicMain.java
@@ -0,0 +1,16 @@
+package musicplaylist;
+
+/**
+ *
+ * @author Mustafa Shubbar <codingbox@outlook.com>
+ */
+public class MusicMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
